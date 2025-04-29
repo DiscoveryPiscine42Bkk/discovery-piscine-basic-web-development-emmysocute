726c00b7da1e775eb299b61fd5007fc17b0196f0
EmmySoCute <yuttaphonp23@gmail.com>
2025-04-29 20:23:01 +0700

    eee

--- cell03/ex00/background.js
+++ cell03/ex00/background.js
@@ -2,6 +2,5 @@ function ex00(){
     let A = Math.floor((Math.random()*255))
     let B = Math.floor((Math.random()*255))
     let C = Math.floor((Math.random()*255));
-
-    document.getElementById("1").style.backgroundColor = "rgb(" + A +"," + B + "," + C  + ")";
+    document.getElementById("1").style.backgroundColor = `rgb(${A},${B},${C},)`
 }
--- cell03/ex01/grown.js
+++ cell03/ex01/grown.js
@@ -1,16 +1,45 @@
+
+const color = ["red","green","blue"]
+let i =0;
+let size = 10;
+let P =document.getElementById("D");
+
 function ex(){
-    const color = ["Red","green","blue","black"]
-    let size = 10;
-    let P =document.getElementById("D");
     let size_1 =P.offsetWidth
     let size_2 =P.offsetHeight
     let N = size+size_1
     let R = size +size_2
     P.style.width = N+"px";
     P.style.height = R+"px";
-    
-    
-    
+    i++;
+    if(i >2){
+        i = 0;
+    }
+    P.style.backgroundColor = color[i]
+    console.log(size_1 +" "+size_2)
+    if(size_1 >= 420){
+    P.style.width = 200+"px";
+    P.style.height = 200+"px";
+    }
+}
 
-
+function boxA(){
+    
+    let size_1 =P.offsetWidth
+    let size_2 =P.offsetHeight
+   
+    let w  = size_1-5;
+    let h = size_2-5;
+    i--;
+    if(i < 0){
+        i = 2;
+    }
+    P.style.backgroundColor = color[i]
+    P.style.height = h+"px";
+    P.style.width = w +"px";
+    if(size_1 < 200 || size_2 < 200){
+        P.style.width = 200+"px";
+        P.style.height = 200+"px";
+    }
+    
 }
